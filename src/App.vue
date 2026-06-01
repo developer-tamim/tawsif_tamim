df74584d4fbbf90279a84b914015766a554d8574
developer-tamim <tawsiftamim24@gmail.com>
2026-06-01 20:18:23 +0600

    padding fixing

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative p-10 px-20 overflow-hidden">
+  <div class="relative p-10 overflow-hidden">
     <div class="absolute -right-24 top-16 h-72 w-72 rounded-full bg-[#16db65]/20 blur-3xl"></div>
     <router-view />
   </div>
